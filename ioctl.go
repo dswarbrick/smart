86046b9a2c74a325faba26338b6addb2ff77521d
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2017-05-10 20:51:51 +0000

    Scan MegaRAID physical device list

--- ioctl.go
+++ ioctl.go
@@ -10,6 +10,47 @@ package smart
 
 import "syscall"
 
+const (
+	directionNone  = 0
+	directionWrite = 1
+	directionRead  = 2
+
+	numberBits    = 8
+	typeBits      = 8
+	sizeBits      = 14
+	directionBits = 2
+
+	numberMask    = (1 << numberBits) - 1
+	typeMask      = (1 << typeBits) - 1
+	sizeMask      = (1 << sizeBits) - 1
+	directionMask = (1 << directionBits) - 1
+
+	numberShift    = 0
+	typeShift      = numberShift + numberBits
+	sizeShift      = typeShift + typeBits
+	directionShift = sizeShift + sizeBits
+)
+
+// _ioc calculates the ioctl command for the specified direction, type, number and size
+func _ioc(dir, t, nr, size uintptr) uintptr {
+	return (dir << directionShift) | (t << typeShift) | (nr << numberShift) | (size << sizeShift)
+}
+
+// _ior calculates the ioctl command for a read-ioctl of the specified type, number and size
+func _ior(t, nr, size uintptr) uintptr {
+	return _ioc(directionRead, t, nr, size)
+}
+
+// _iow calculates the ioctl command for a write-ioctl of the specified type, number and size
+func _iow(t, nr, size uintptr) uintptr {
+	return _ioc(directionWrite, t, nr, size)
+}
+
+// _iowr calculates the ioctl command for a read/write-ioctl of the specified type, number and size
+func _iowr(t, nr, size uintptr) uintptr {
+	return _ioc(directionWrite|directionRead, t, nr, size)
+}
+
 // ioctl executes an ioctl command on the specified file descriptor
 func ioctl(fd, cmd, ptr uintptr) error {
 	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, ptr)
--- megaraid.go
+++ megaraid.go
@@ -2,8 +2,9 @@
  * Pure Go SMART library
  * Copyright 2017 Daniel Swarbrick
  *
- * Avago MegaRAID ioctl functions
+ * Broadcom (formerly Avago, LSI) MegaRAID ioctl functions
  * TODO:
+ * - Improve code comments, refer to in-kernel structs
  * - Device Scan:
  *   - Walk /sys/class/scsi_host/ directory
  *   - "host%d" symlinks enumerate hosts
@@ -14,23 +15,99 @@ package smart
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"syscall"
+	"unsafe"
 )
 
+const (
+	MAX_IOCTL_SGE = 16
+
+	MFI_CMD_DCMD = 0x05
+
+	MR_DCMD_PD_GET_LIST = 0x02010000 // Obsolete / deprecated command
+)
+
+type megasas_sge64 struct {
+	phys_addr uint32
+	length    uint32
+	_padding  uint32
+}
+
+type Iovec struct {
+	Base uint64 // FIXME: This is not portable to 32-bit platforms!
+	Len  uint64
+}
+
+type megasas_dcmd_frame struct {
+	cmd           uint8
+	reserved_0    uint8
+	cmd_status    uint8
+	reserved_1    [4]uint8
+	sge_count     uint8
+	context       uint32
+	pad_0         uint32
+	flags         uint16
+	timeout       uint16
+	data_xfer_len uint32
+	opcode        uint32
+	mbox          [12]byte      // FIXME: This is actually a union of [12]uint8 / [6]uint16 / [3]uint32
+	sgl           megasas_sge64 // FIXME: This is actually a union of megasas_sge64 / megasas_sge32
+}
+
+type megasas_iocpacket struct {
+	host_no   uint16
+	__pad1    uint16
+	sgl_off   uint32
+	sge_count uint32
+	sense_off uint32
+	sense_len uint32
+	// FIXME: This is actually a union of megasas_header / megasas_pthru_frame / megasas_dcmd_frame
+	frame [128]byte
+	// FIXME: Go is inserting 4 bytes of padding before this in order to 64-bit align the sgl member
+	sgl [MAX_IOCTL_SGE]Iovec
+}
+
+// Megasas physical device address
+type MegasasPDAddress struct {
+	DeviceId          uint16
+	EnclosureId       uint16
+	EnclosureIndex    uint8
+	SlotNumber        uint8
+	SCSIDevType       uint8
+	ConnectPortBitmap uint8
+	SASAddr           [2]uint64
+}
+
+// Holder for megasas ioctl device
 type MegasasIoctl struct {
 	DeviceMajor int
 	fd          int
 }
+
+var (
+	// 0xc1944d01 - Beware: cannot use unsafe.Sizeof(megasas_iocpacket{}) due to Go struct padding!
+	MEGASAS_IOC_FIRMWARE = _iowr('M', 1, 404)
+)
 
 // MakeDev returns the device ID for the specified major and minor numbers, equivalent to
 // makedev(3). Based on gnu_dev_makedev macro, may be platform dependent!
 func MakeDev(major, minor uint) uint {
 	return (minor & 0xff) | ((major & 0xfff) << 8) |
 		((minor &^ 0xff) << 12) | ((major &^ 0xfff) << 32)
+}
+
+// PackedBytes is a convenience method that will pack a megasas_iocpacket struct in little-endian
+// format and return it as a byte slice
+func (ioc *megasas_iocpacket) PackedBytes() []byte {
+	b := new(bytes.Buffer)
+	binary.Write(b, binary.LittleEndian, ioc)
+	return b.Bytes()
 }
 
 // CreateMegasasIoctl determines the device ID for the MegaRAID SAS ioctl device, creates it
@@ -79,11 +156,66 @@ func (m *MegasasIoctl) Close() {
 	syscall.Close(m.fd)
 }
 
+// MFI sends a MegaRAID Firmware Interface (MFI) command to the specified host
+func (m *MegasasIoctl) MFI(host uint16, opcode uint32, b []byte) error {
+	var ioc megasas_iocpacket
+
+	ioc.host_no = host
+
+	// Approximation of C union behaviour
+	dcmd := (*megasas_dcmd_frame)(unsafe.Pointer(&ioc.frame))
+	dcmd.cmd = MFI_CMD_DCMD
+	dcmd.opcode = opcode
+	dcmd.data_xfer_len = uint32(len(b))
+	dcmd.sge_count = 1
+
+	ioc.sge_count = 1
+	ioc.sgl_off = uint32(unsafe.Offsetof(dcmd.sgl))
+	ioc.sgl[0] = Iovec{uint64(uintptr(unsafe.Pointer(&b[0]))), uint64(len(b))}
+
+	iocBuf := ioc.PackedBytes()
+
+	// Note pointer to first item in iocBuf buffer
+	if err := ioctl(uintptr(m.fd), MEGASAS_IOC_FIRMWARE, uintptr(unsafe.Pointer(&iocBuf[0]))); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetDeviceList retrieves a list of physical devices attached to the specified host
+func (m *MegasasIoctl) GetDeviceList(host uint16) ([]MegasasPDAddress, error) {
+	respBuf := make([]byte, 4096)
+
+	if err := m.MFI(0, MR_DCMD_PD_GET_LIST, respBuf); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	respCount := nativeEndian.Uint32(respBuf[4:])
+
+	// Create a device array large enough to hold the specified number of devices
+	devices := make([]MegasasPDAddress, respCount)
+	binary.Read(bytes.NewBuffer(respBuf[8:]), nativeEndian, &devices)
+
+	return devices, nil
+}
+
 func OpenMegasasIoctl() error {
 	m, _ := CreateMegasasIoctl()
 	fmt.Printf("%#v\n", m)
 
 	defer m.Close()
+
+	// FIXME: Don't assume that host is always zero
+	devices, _ := m.GetDeviceList(0)
+
+	fmt.Println("\nEncl.  Slot  Device Id  SAS Address")
+	for _, pd := range devices {
+		if pd.SCSIDevType == 0 { // SCSI disk
+			fmt.Printf("%5d   %3d      %5d  %#x\n", pd.EnclosureId, pd.SlotNumber, pd.DeviceId, pd.SASAddr[0])
+		}
+	}
 
 	return nil
 }
