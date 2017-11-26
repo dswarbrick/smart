011f8a07275a272cdbb38bb0a08d88f9e1d3b21d
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2017-11-26 01:15:42 +0100

    Use bits.Len() from math/bits package in Go 1.9

--- bitops.go
+++ bitops.go
@@ -1,39 +1,18 @@
 // Copyright 2017 Daniel Swarbrick. All rights reserved.
 // Use of this source code is governed by a GPL license that can be found in the LICENSE file.
-//
-// Portions Copyright 2017 The Go Authors. All rights reserved.
 
 // Miscellaneous bit operations.
 
 package smart
 
-import "fmt"
-
-func bitLen(x uint) (n uint) {
-	for ; x >= 0x8000; x >>= 16 {
-		n += 16
-	}
-	if x >= 0x80 {
-		x >>= 8
-		n += 8
-	}
-	if x >= 0x8 {
-		x >>= 4
-		n += 4
-	}
-	if x >= 0x2 {
-		x >>= 2
-		n += 2
-	}
-	if x >= 0x1 {
-		n++
-	}
-	return
-}
+import (
+	"fmt"
+	"math/bits"
+)
 
 // log2b finds the most significant bit set in a uint.
-func log2b(x uint) uint {
-	return bitLen(x) - 1
+func log2b(x uint) int {
+	return bits.Len(x) - 1
 }
 
 // swapBytes swaps the order of every second byte in a byte slice (modifies slice in-place).
