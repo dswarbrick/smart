bdfd4f7cf680b37d4c71790eb21f0163dcb97019
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2018-04-14 19:55:13 +0200

    Add placeholder for nvme & megaraid subpackages

---
+++ megaraid/megaraid.go
@@ -0,0 +1,6 @@
+// Copyright 2017-18 Daniel Swarbrick. All rights reserved.
+// Use of this source code is governed by a GPL license that can be found in the LICENSE file.
+
+// Placeholder
+
+package megaraid
